fe698b56780e739bc342518fb6664b031a7eea81
MamathaJaldu <88569465+MamathaJaldu@users.noreply.github.com>
2024-01-07 14:46:31 -0500

    tfvars added

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "tfec2-state-dev"
+key    = "multienv"
+region = "us-east-1"
+dynamodb_table = "tfec2-locking-dev"
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "tfec2-state-prod"
+key    = "multienv"
+region = "us-east-1"
+dynamodb_table = "tfec2-locking-prod"
